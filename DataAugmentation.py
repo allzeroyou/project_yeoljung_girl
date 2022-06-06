2a97983bf6840b5ff4e9049b82aa6b9886c51534
allzeroyou <ekdudd119@naver.com>
2022-06-06 23:49:07 +0900

    [MODIFY] 이미지 어그멘테이션 기존 파일

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -3,16 +3,20 @@
 작성자 : 유다영
 작성일 : 2022.06.02
 기타: 회전, 뒤집기 등 훈련 데이터의 폭을 넓히기 위한 어그멘테이션 기능입니동
-하지만 아직 손 봐야 합니다..
+- 손 봐야 할 것
+train > 2의 폴더 전체를 돌렸는데, 출력결과는 _rotate.png 파일한개뿐?
+여러 파일이 생기지 않는 이유?
+
 - 6월 5일 업데이트
 코드봤지만 아직도 왜 1개 파일만 생기는지 모르겠다.
 save 함수를 어떻게 해야 할 듯 한데, 파라미터가 많아서 손보기가 어렵다.
 내일 다시 도전
 할 수 있다!
 
-- 손 봐야 할 것
-train > 2의 폴더 전체를 돌렸는데, 출력결과는 _rotate.png 파일한개뿐?
-여러 파일이 생기지 않는 이유?
+- 6월 6일 업데이트
+이미지어그멘테이션 성공~!
+아래 코드 말고 주피터노트북에서 새롭게 코드 짰어요
+파일 경로: dataset > handwriting_number > DataAugmentation.ipynb(주피터노트북 파일)
 
 출처: https://github.com/oryondark/hjkim/blob/master/DeepLearning_master/Augmentation_Tutorial/Data%20Augmentation.md
 '''
@@ -25,7 +29,7 @@ import os, sys
 
 # image parse in directory
 
-root_dir_path = './dataset/handwriting_number/train/2'  # target images directory
+root_dir_path = './dataset/handwriting_number/train/19'  # target images directory
 root_dir = os.listdir(root_dir_path)
 
 # search = '.png'
@@ -38,17 +42,7 @@ print(root_dir)  # 폴더 내 파일명으로 오름차순 정렬
 
 
 # Create Save method by openCV
-# keyPath는 원본 이미지의 루트 경로
-# file_name은 원본 이미지 파일 이름
-# cv_img는 이미지의 전체 신호
-# 비율은 스케일 값에 대한 것
-def save(keyPath, file_name, cv_img, rate, type):
-    if not os.path.isdir(keyPath):
-        os.mkdir(keyPath)
 
-    saved_name = os.path.join(keyPath, "{}{}.{}".format(file_name.split('.')[0], type, 'png'))
-    print(saved_name)
-    cv.imwrite(saved_name, cv_img)
 
 
 # 1.  Scaling
@@ -57,13 +51,14 @@ def augmente(keyName, rate=None, if_scale=False):
     keyPath = os.path.join(root_dir_path, keyName)  # keypath direct to root path
     keyPath_rep = keyPath.replace('\\', '/')
     keyPath_rep_list = []
-    i=0
-    while len(keyPath_rep_list) <= 9:
-        for i in range(10):
-            keyPath_rep_list.append(keyPath_rep[:39]+str(i)+'.png')
-            print("keyPath: ", keyPath_rep_list[i])
-            # keyPath_rep += 1
-            print(len(keyPath_rep_list))
+
+    i = 0
+    while i <= 9:
+        keyPath_rep_list.append(str(i) + '.png')
+        print("keyPath: ", keyPath_rep_list[i])
+        i += 1
+        print(len(keyPath_rep_list))
+
     datas = keyPath_rep_list
     data_total_num = len(datas)
 
@@ -76,7 +71,7 @@ def augmente(keyName, rate=None, if_scale=False):
             img = cv.imread(keyPath_rep)
             shape = img.shape
             ###### data rotate ######
-            data_rotate(saved_dir, data, img, 20, "_rotate_", saving_enable=True)
+            data_rotate(saved_dir, data, img, 20, "_rotate_", saving_enable=True) # 숫자는 회전할 각도
 
             ###### data flip and save #####
             data_flip(saved_dir, data, img, rate, 1, False)  # verical random flip
@@ -84,7 +79,7 @@ def augmente(keyName, rate=None, if_scale=False):
             data_flip(saved_dir, data, img, rate, -1, False)  # both random flip
 
             ####### Image Scale #########
-            if if_scale == True:
+            if if_scale:
                 print("Start Scale!")
                 x = shape[0]
                 y = shape[1]
@@ -94,7 +89,15 @@ def augmente(keyName, rate=None, if_scale=False):
 
                 img = img[0:y, 0:x]
 
-                save(saved_dir, data, img, rate, type)
+                if not os.path.isdir(saved_dir):
+                    os.mkdir(saved_dir)
+                saved_name = os.path.join(saved_dir, "{}{}.{}".format(i, type, 'png'))
+                print(saved_name)
+                cv.imwrite(saved_name, img)
+
+
+                # save(saved_dir, data, img, rate, type)
+
             ############################
 
         plt.imshow(img)
@@ -114,13 +117,19 @@ def data_rotate(saved_dir, data, img, rate, type, saving_enable=False):
     try:
         rotation_matrix = cv.getRotationMatrix2D((xLength / 2, yLength / 2), rate, 1)
         img = cv.warpAffine(img, rotation_matrix, (xLength, yLength))
-        # print(img.shape)
+        print(img.shape)
+
         if saving_enable == True:
-            i = 0
-            for i in range(10):
-                save(saved_dir, data, img, rate, type)
+            if not os.path.isdir(saved_dir):
+                os.mkdir(saved_dir)
+            saved_name = os.path.join(saved_dir, "{}{}.{}".format(data, type, 'png'))
+            print(saved_name)
+            cv.imwrite(saved_name, img)
+
+                # save(saved_dir, data, img, rate, type)
 
         return "Success"
+
     except Exception as e:
         print(e)
         return "Failed"
@@ -137,7 +146,12 @@ def data_flip(saved_dir, data, img, rate, type, saving_enable=False):
             type = "_bothFlip_"
 
         if saving_enable == True:
-            save(saved_dir, data, img, rate, type)
+            if not os.path.isdir(saved_dir):
+                os.mkdir(saved_dir)
+            saved_name = os.path.join(saved_dir, "{}{}.{}".format(data.split('.')[0], type, 'png'))
+            print(saved_name)
+            cv.imwrite(saved_name, img)
+            # save(saved_dir, data, img, rate, type)
 
     except Exception as e:
         print(e)
